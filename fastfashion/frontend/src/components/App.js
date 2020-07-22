a930ab7a014224c107adbba990642651d3da3448
IshitaB08 <f20180306@pilani.bits-pilani.ac.in>
2020-07-22 06:01:24 +0530

    django auth2

--- fastfashion/frontend/src/components/App.js
+++ fastfashion/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -22,13 +23,17 @@ class App extends Component {
         return (
             <Provider store={store}>
             <AlertProvider template={AlertTemplate}{...alertOptions}>
+                <Router>
                 <Fragment>
                     <Header />
                     <Alerts />
                     <div className="container">
-                    <Dashboard />
-                    </div>
+                    <Switch>
+                        <Route exact path="/" component={Dashboard} />
+                    </Switch>
+                   </div>
                 </Fragment>
+                </Router>
             </AlertProvider>
             </Provider>
         )
