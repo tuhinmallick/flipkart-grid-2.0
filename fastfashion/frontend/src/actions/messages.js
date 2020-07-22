66140a865bbeab5c4d530558b2d33fff34d6d73d
IshitaB08 <f20180306@pilani.bits-pilani.ac.in>
2020-07-22 05:43:05 +0530

    django auth

--- fastfashion/frontend/src/actions/messages.js
+++ fastfashion/frontend/src/actions/messages.js
@@ -1,9 +1,17 @@
-import { CREATE_MESSAGE, CREAT_MESSAGE } from './types';
+import { CREATE_MESSAGE, GET_ERRORS } from './types';
 
 // CREATE MESSAGE
 export const createMessage = msg => {
     return {
         type: CREAT_MESSAGE,
         payload: msg
+    }
+}
+
+// Reutrn errors
+export const returnErrors = (msg, status) => {
+    return {
+        type: GET_ERRORS,
+        payload: { msg, status }
     }
 }
